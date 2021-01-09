a64b6c71b2fd907b4087d56da706a3d24794b1a0
Jeroen-PC\Jeroen <jeroenlogger@gmail.com>
2021-01-09 15:13:37 +0100

    working version

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+i
+9app/src/main/java/nl/hva/level6example/ui/MainActivity.kt,e\7\e7f2dc2d1713911b699583d4e4936297edc0017d
---
+++ app/src/main/java/nl/hva/level6example/ui/MainActivity.kt
@@ -0,0 +1,44 @@
+package nl.hva.level6example.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import nl.hva.level6example.R
+import nl.hva.level6example.databinding.ActivityMainBinding
+import nl.hva.level6example.vm.TriviaViewModel
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel: TriviaViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener {
+            viewModel.getTriviaNumber()
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
